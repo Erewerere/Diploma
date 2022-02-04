e8ed724b2d19179f4d3cb60672bf41b12f84bd6b
Erewerere <parad0304@gmail.com>
2022-02-04 22:22:51 +0200

    added menu items, tested excel without success, added mainwindow

--- Models/ExcelGenerator.cs
+++ Models/ExcelGenerator.cs
@@ -1,26 +1,16 @@
-﻿//using OfficeOpenXml;
-//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel.DataAnnotations;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+﻿using OfficeOpenXml;
 
-//namespace Diploma.Models
 
-//{
-//    public class ExcelGenerator
-//    {
+namespace Diploma.Models
 
-//        public ExcelGenerator()
-//        {
-//        }
-//        public byte[] Generate(Service service)
-//        {
-//            var package = new ExcelPackage();
-//            var sheet = package.Workbook.Worksheets.Add("Repoert");
-//            return package.GetAsByteArray();
-//        }
-//    }
-//}
+{
+    public class ExcelGenerator
+    {      
+        public byte[] Generate(Service service)
+        {
+            var package = new ExcelPackage();
+            var sheet = package.Workbook.Worksheets.Add("Repoert");
+            return package.GetAsByteArray();
+        }
+    }
+}
--- Models/SocialIntegration.cs
+++ Models/SocialIntegration.cs
@@ -11,6 +11,6 @@ namespace Diploma.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Type { get; set; }
+        public string Name { get; set; }
     }
 }
